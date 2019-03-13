52609e0c524971ecb9e611707919423ba35c86c8
manoj-makkuboy <jesuismanoj@gmail.com>
2019-03-13 20:45:04 +0530

    Create webview and show https://news.ycombinator.com

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,32 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import axios from 'axios';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "hacker-news" is now active!');
+	let disposable = vscode.commands.registerCommand('extension.hackerNews', async () => {
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+		const panel = vscode.window.createWebviewPanel(
+			'hackerNews', // Identifies the type of the webview. Used internally
+			'Hacker News', // Title of the panel displayed to the user
+			vscode.ViewColumn.Two, // Editor column to show the new webview panel in.
+			{
+				enableScripts: true,
+				enableFindWidget: true,
+				retainContextWhenHidden: true,
+			 }
+		);
+		
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+			let response = await axios.get('https://news.ycombinator.com');
+			panel.webview.html = response.data;
+
+
 	});
 
 	context.subscriptions.push(disposable);
+
+
+	
 }
 
 // this method is called when your extension is deactivated
